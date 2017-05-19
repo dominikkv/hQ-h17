28c364cd0e325e6ca2f407e6e2969cb086df60dd
DominikGebhart <DominikGebhart@users.noreply.github.com>
2017-05-19 23:05:17 +0200

    Weitere ActiveAndroid Datenbankdefinitionen zugefügt

---
+++ Qonverter/app/src/main/java/eu/highq/qonverter/database/Category.java
@@ -0,0 +1,13 @@
+package eu.highq.qonverter.database;
+
+import com.activeandroid.Model;
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
+
+@Table(name = "tbl_Categories")
+public class Category extends Model {
+
+    @Column(name = "Name")
+    public String name;
+
+}
