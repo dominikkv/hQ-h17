886cee10d98d61b615005498f4f1ca5b625564c2
Johannes Mols <johannes.mols@googlemail.com>
2017-05-20 12:13:15 +0100

    Back Button for Item Selection Deleted activity_act_choose

--- Qonverter/app/src/main/java/eu/highq/qonverter/ItemSelectionActivity.java
+++ Qonverter/app/src/main/java/eu/highq/qonverter/ItemSelectionActivity.java
@@ -1,9 +1,10 @@
 package eu.highq.qonverter;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.content.Intent;
 import android.widget.Toast;
 
 public class ItemSelectionActivity extends AppCompatActivity {
@@ -15,6 +16,9 @@ public class ItemSelectionActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle(R.string.item_selection_toolbar_title);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
 
         Intent intent = getIntent();
         String value = intent.getStringExtra("item");
