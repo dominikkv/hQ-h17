c06ec08ea5ec5a3333ba2578d0f9cb6f24851e3d
Johannes Mols <johannes.mols@googlemail.com>
2017-05-20 18:06:22 +0100

    Obsolte imports entfernt

--- Qonverter/app/src/main/java/eu/highq/qonverter/ActCompare.java
+++ Qonverter/app/src/main/java/eu/highq/qonverter/ActCompare.java
@@ -1,14 +1,11 @@
 package eu.highq.qonverter;
 
 import android.os.Bundle;
-import android.provider.Telephony;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.content.Intent;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -18,13 +15,9 @@ import android.widget.Spinner;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.activeandroid.query.Select;
-
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
-
 import eu.highq.qonverter.database.Category;
 import eu.highq.qonverter.database.EnergyCarrier;
 import eu.highq.qonverter.database.Unit;
