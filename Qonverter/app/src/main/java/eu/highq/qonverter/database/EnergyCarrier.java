f82efebb2d00bb916b45d796a27045d92a8fd973
sam <sam.probst@gmail.com>
2017-05-20 17:19:34 +0200

    Listener for ItemSelect

--- Qonverter/app/src/main/java/eu/highq/qonverter/database/EnergyCarrier.java
+++ Qonverter/app/src/main/java/eu/highq/qonverter/database/EnergyCarrier.java
@@ -39,6 +39,8 @@ public class EnergyCarrier extends Model {
         Category categoryLebensmittel = new Select().from(Category.class).where("Name = ?", "Lebensmittel").executeSingle();
         Category categoryKraftwerk = new Select().from(Category.class).where("Name = ?", "Kraftwerke").executeSingle();
         Category categoryBewegung = new Select().from(Category.class).where("Name = ?", "Bewegung").executeSingle();
+        Category categoryBrennstoffe = new Select().from(Category.class).where("Name = ?", "Brennstoffe").executeSingle();
+        Category categoryHaushaltsgeraete = new Select().from(Category.class).where("Name = ?", "Haushaltsgeräte").executeSingle();
 
         EnergyCarrier carrier = new EnergyCarrier();
         carrier.name = "Auto";
@@ -72,6 +74,141 @@ public class EnergyCarrier extends Model {
         carrier.description = "Wie weit muss man mit dem Rad fahren?";
         carrier.save();
 
+        carrier = new EnergyCarrier();
+        carrier.name = "Wasserstoff";
+        carrier.category = categoryBrennstoffe;
+        carrier.energy = 142000;
+        carrier.unit = unitGewicht;
+        carrier.description = "Brennwert von Wasserstoff";
+        carrier.save();
+
+        carrier = new EnergyCarrier();
+        carrier.name = "Kohlenmonoxid";
+        carrier.category = categoryBrennstoffe;
+        carrier.energy = 10100;
+        carrier.unit = unitGewicht;
+        carrier.description = "Brennwert von Kohlenmonoxid";
+        carrier.save();
+
+        carrier = new EnergyCarrier();
+        carrier.name = "Methan";
+        carrier.category = categoryBrennstoffe;
+        carrier.energy = 55400;
+        carrier.unit = unitGewicht;
+        carrier.description = "Brennwert von Methan";
+        carrier.save();
+
+        carrier = new EnergyCarrier();
+        carrier.name = "Propan";
+        carrier.category = categoryBrennstoffe;
+        carrier.energy = 50300;
+        carrier.unit = unitGewicht;
+        carrier.description = "Brennwert von Propan";
+        carrier.save();
+
+        carrier = new EnergyCarrier();
+        carrier.name = "Erdgas L";
+        carrier.category = categoryBrennstoffe;
+        carrier.energy = 42400;
+        carrier.unit = unitGewicht;
+        carrier.description = "Brennwert von Erdgas L";
+        carrier.save();
+
+        carrier = new EnergyCarrier();
+        carrier.name = "Erdgas H";
+        carrier.category = categoryBrennstoffe;
+        carrier.energy = 52300;
+        carrier.unit = unitGewicht;
+        carrier.description = "Brennwert von Erdgas H";
+        carrier.save();
+
+        carrier = new EnergyCarrier();
+        carrier.name = "HeizÖl EÖ";
+        carrier.category = categoryBrennstoffe;
+        carrier.energy = 45400;
+        carrier.unit = unitGewicht;
+        carrier.description = "Brennwert von HeizÖl EÖ";
+        carrier.save();
+
+        carrier = new EnergyCarrier();
+        carrier.name = "Heizöl S";
+        carrier.category = categoryBrennstoffe;
+        carrier.energy = 43300;
+        carrier.unit = unitGewicht;
+        carrier.description = "Brennwert von Heizöl S";
+        carrier.save();
+
+        carrier = new EnergyCarrier();
+        carrier.name = "Diesel";
+        carrier.category = categoryBrennstoffe;
+        carrier.energy = 45400;
+        carrier.unit = unitGewicht;
+        carrier.description = "Brennwert von Diesel";
+        carrier.save();
+
+        carrier = new EnergyCarrier();
+        carrier.name = "Benzin";
+        carrier.category = categoryBrennstoffe;
+        carrier.energy = 46500;
+        carrier.unit = unitGewicht;
+        carrier.description = "Brennwert von Benzin";
+        carrier.save();
+
+        carrier = new EnergyCarrier();
+        carrier.name = "Methanol";
+        carrier.category = categoryBrennstoffe;
+        carrier.energy = 22700;
+        carrier.unit = unitGewicht;
+        carrier.description = "Brennwert von Methanol";
+        carrier.save();
+
+        carrier = new EnergyCarrier();
+        carrier.name = "Ethanol";
+        carrier.category = categoryBrennstoffe;
+        carrier.energy = 29700;
+        carrier.unit = unitGewicht;
+        carrier.description = "Brennwert von Ethanol";
+        carrier.save();
+
+        carrier = new EnergyCarrier();
+        carrier.name = "Graphit";
+        carrier.category = categoryBrennstoffe;
+        carrier.energy = 33800;
+        carrier.unit = unitGewicht;
+        carrier.description = "Brennwert von Graphit";
+        carrier.save();
+
+        carrier = new EnergyCarrier();
+        carrier.name = "Steinkohle";
+        carrier.category = categoryBrennstoffe;
+        carrier.energy = 31700;
+        carrier.unit = unitGewicht;
+        carrier.description = "Brennwert von Steinkohle";
+        carrier.save();
+
+        carrier = new EnergyCarrier();
+        carrier.name = "Koks";
+        carrier.category = categoryBrennstoffe;
+        carrier.energy = 28900;
+        carrier.unit = unitGewicht;
+        carrier.description = "Brennwert von Koks";
+        carrier.save();
+
+        carrier = new EnergyCarrier();
+        carrier.name = "Rohbraunkohle";
+        carrier.category = categoryBrennstoffe;
+        carrier.energy = 10500;
+        carrier.unit = unitGewicht;
+        carrier.description = "Brennwert von Rohbraunkohle";
+        carrier.save();
+
+        carrier = new EnergyCarrier();
+        carrier.name = "Holz(trocken)";
+        carrier.category = categoryBrennstoffe;
+        carrier.energy = 20000;
+        carrier.unit = unitGewicht;
+        carrier.description = "Holz(trocken)";
+        carrier.save();
 
     }
 }
