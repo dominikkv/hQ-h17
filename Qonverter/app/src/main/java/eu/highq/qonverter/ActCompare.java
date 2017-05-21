d5364d8ad49cf74efcebd4e7cd63e701956279cc
Johannes Mols <johannes.mols@googlemail.com>
2017-05-21 11:29:07 +0200

    removed an import

--- Qonverter/app/src/main/java/eu/highq/qonverter/ActCompare.java
+++ Qonverter/app/src/main/java/eu/highq/qonverter/ActCompare.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.content.Intent;
