2466db4cbbd11299311351abd16d9d89b9b5f5bb
dominikkv <dominikkv@gmx.de>
2017-05-21 10:10:04 +0200

    Fix for abbreviation calculation

--- Qonverter/app/src/main/java/eu/highq/qonverter/models/CompareItem.java
+++ Qonverter/app/src/main/java/eu/highq/qonverter/models/CompareItem.java
@@ -4,24 +4,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 import eu.highq.qonverter.database.EnergyCarrier;
+import eu.highq.qonverter.database.UnitAbbreviation;
 import eu.highq.qonverter.database.Variant;
 
 public class CompareItem {
     public EnergyCarrier carrier;
     public List<Variant> variants;
     public double factor;
+    public UnitAbbreviation abbr;
 
     public CompareItem(EnergyCarrier carrier) {
         this.variants = new ArrayList<>();
         this.factor = 1.0;
         this.carrier = carrier;
+        this.abbr = carrier.unit.abbreviations().get(0);
     }
 
     public Long calculateEnergy() {
         Double energy = carrier.energy;
 
         for (Variant variant : this.variants) {
-            energy = (Double) (energy * variant.factor);
+            energy = (energy * variant.factor);
         }
 
         return (long) (energy * this.factor);
@@ -31,7 +34,7 @@ public class CompareItem {
         Double ownEnergy = carrier.energy;
 
         for (Variant variant : this.variants) {
-            ownEnergy = (Double) (ownEnergy * variant.factor);
+            ownEnergy = (ownEnergy * variant.factor);
         }
 
         this.factor = energy.doubleValue() / ownEnergy;
